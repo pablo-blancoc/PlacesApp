04ba992ea9259a58e8edfb4cae8e98456e07c375
Pablo Blanco <pabloblanco@fb.com>
2021-07-12 12:50:12 -0500

    Implemented LoginActivity [not done]
    1. Changed color scheme to my app colors
    2. Added ViewBinding to reduce boilerplate code
    3. Started to implement activity's code

--- Places/app/src/main/java/com/example/places/LoginActivity.java
+++ Places/app/src/main/java/com/example/places/LoginActivity.java
@@ -1,14 +1,25 @@
 package com.example.places;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.places.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        // Setup ViewBinding
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
