0d79e840108b55cc49f8235076b0f347ab4f41b6
Pablo Blanco <pabloblanco@fb.com>
2021-07-13 16:38:51 -0500

    take picture implemented

--- Places/app/src/main/java/com/example/places/CreatePlace.java
+++ Places/app/src/main/java/com/example/places/CreatePlace.java
@@ -2,12 +2,20 @@ package com.example.places;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Looper;
 import android.text.TextUtils;
@@ -16,6 +24,7 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.places.databinding.ActivityCreatePlaceBinding;
 import com.example.places.databinding.ActivityLoginBinding;
 import com.example.places.databinding.ActivityMainBinding;
@@ -38,8 +47,15 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.PermissionUtils;
@@ -51,6 +67,8 @@ public class CreatePlace extends AppCompatActivity {
     // Constants
     private final static String TAG = "CreatePlace";
     private final static String KEY_LOCATION = "location";
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
 
     // Attributes
     private ActivityCreatePlaceBinding binding;
@@ -58,6 +76,9 @@ public class CreatePlace extends AppCompatActivity {
     private GoogleMap map;
     private PermissionsDispatcher dispatcher;
     private Boolean addedMarker = false;
+    ImageCapture imageCapture;
+    File outputDirectory;
+    ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +104,25 @@ public class CreatePlace extends AppCompatActivity {
             });
         } else {
             Toast.makeText(this, "Error loading map", Toast.LENGTH_SHORT).show();
+        }
+
+        // Listener to take picture
+        binding.btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+
+        // setting up the output directory and creating new thread for taking pictures
+        outputDirectory = getOutputDirectory();
+        cameraExecutor = Executors.newSingleThreadExecutor();
+
+        // Start camera
+        if(hasCameraPermission()) {
+            enableCamera();
+        } else {
+            requestCameraPermission();
         }
     }
 
@@ -110,6 +150,14 @@ public class CreatePlace extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if(dispatcher != null) {
             dispatcher.onRequestPermissionsResult(requestCode, grantResults);
+        }
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                enableCamera();
+            } else {
+                Toast.makeText(this, "Permissions not grated by the user", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
@@ -174,5 +222,117 @@ public class CreatePlace extends AppCompatActivity {
         });
     }
 
+    /**
+     * Returns whether the user has granted access to use the camera
+     * @return boolean
+     */
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * Requests the user the permission to use the camera
+     */
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, CAMERA_REQUEST_CODE);
+    }
+
+    /**
+     * Takes a picture using CameraX
+     */
+    private void takePhoto() {
+        if(imageCapture == null){
+            return;
+        }
+
+        // set up the photo file for storing the photo
+        File photoFile = new File(outputDirectory, new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.US).format(System.currentTimeMillis()) + ".jpg");
+
+        // save the image and wait for callback.
+        ImageCapture.OutputFileOptions fileOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+        imageCapture.takePicture(fileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                Uri savedUri = Uri.fromFile(photoFile);
+                String msg = "Photo Capture succeeded " + savedUri.toString();
+                Log.d(TAG, msg);
+
+                // View image on page and take our button to take picture
+                binding.btnImage.setVisibility(View.GONE);
+                binding.pvImage.setVisibility(View.GONE);
+                binding.ivImage.setVisibility(View.VISIBLE);
+
+                // Save image into File variable
+                File image = new File(savedUri.getPath());
+
+                // Load image with Glide
+                Glide.with(CreatePlace.this)
+                        .load(new File(savedUri.getPath()))
+                        .into(binding.ivImage);
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                Toast.makeText(CreatePlace.this, "Image could not be saved. Try again later.", Toast.LENGTH_SHORT).show();
+                Log.i("Image Capture", exception.toString());
+            }
+        });
+    }
+
+    /**
+     * Enables the camera in order to take a picture
+     */
+    private void enableCamera() {
+        ListenableFuture<ProcessCameraProvider> processCameraProvider = ProcessCameraProvider.getInstance(this);
+
+        // the main logic is in a listener.
+        processCameraProvider.addListener(new Runnable() {
+            @Override
+            public void run() {
+                ProcessCameraProvider cameraProvider = null;
+                try {
+                    cameraProvider = processCameraProvider.get();
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception on enableCamera", e);
+                }
+
+                // set up preview window
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(binding.pvImage.createSurfaceProvider());
+
+                imageCapture = new ImageCapture.Builder().build();
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                // tie the preview, camera selector and imageCapture together via the cameraProvider
+                try {
+                    cameraProvider.unbindAll();
+                    cameraProvider.bindToLifecycle(CreatePlace.this, cameraSelector, preview, imageCapture);
+                } catch (Exception e){
+                    Log.e(TAG, "Exception on enableCamera", e);
+                }
+            }
+
+
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    /**
+     * Gets the output directory of the picture if it is to be taken
+     * @return File
+     */
+    private File getOutputDirectory() {
+
+        File mediaDirs = getExternalMediaDirs()[0];
+        File newFiles = null;
+
+        if (mediaDirs != null) {
+            newFiles = new File(mediaDirs, getResources().getString(R.string.app_name));
+            newFiles.mkdirs();
+        }
+        if (newFiles != null && mediaDirs.exists()) {
+            return mediaDirs;
+        } else
+            return getFilesDir();
+    }
 
 }
