a7b6b88893b304621945483c2712f7255bc575ec
Pablo Blanco <pabloblanco@fb.com>
2021-07-15 14:49:32 -0500

    updating name and bio done; implementing updating profile picture

--- Places/app/src/main/java/com/example/places/ui/profile/ProfileFragment.java
+++ Places/app/src/main/java/com/example/places/ui/profile/ProfileFragment.java
@@ -1,38 +1,222 @@
 package com.example.places.ui.profile;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.places.ProfileActivity;
 import com.example.places.R;
+import com.example.places.adapters.ProfilePlacesAdapter;
+import com.example.places.adapters.SearchResultsAdapter;
+import com.example.places.databinding.ActivityProfileBinding;
 import com.example.places.databinding.LikedFragmentBinding;
 import com.example.places.databinding.ProfileFragmentBinding;
+import com.example.places.models.Place;
+import com.example.places.models.User;
 import com.example.places.ui.liked.LikedViewModel;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
+    // Constants
+    private final static String TAG = "ProfileFragment";
+
+    // Attributes
     private ProfileViewModel profileViewModel;
     private ProfileFragmentBinding binding;
+    private Context context;
+    private List<Place> places;
+    private ProfilePlacesAdapter adapter;
+    User user;
+    int followerCount = 0, followingCount = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = ProfileFragmentBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View root = this.binding.getRoot();
+        this.context = getContext();
+
+        // Create adapter and set it
+        this.places = new ArrayList<>();
+        this.adapter = new ProfilePlacesAdapter(this.context, this.places);
+        this.binding.rvPlaces.setAdapter(this.adapter);
+        this.binding.rvPlaces.setLayoutManager(new GridLayoutManager(context, 3));
+
+        // Get logged user and fill information
+        this.getUser(ParseUser.getCurrentUser().getObjectId());
+
+        // Add clickListener to modify the name of the user
+        this.binding.tvName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onUpdateTextInformation(context, "Enter new name:", "name");
+            }
+        });
+
+        // Add clickListener to modify the bio of the user
+        this.binding.tvBio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onUpdateTextInformation(context, "Enter new bio:", "bio");
+            }
+        });
+
+        return root;
+    }
+
+    private void changeText(TextView view, String text) {
+        view.setText(text);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    /**
+     * Get the information from the user that wants to be shown and bind the information
+     * @param uid: The user that wants to be shown
+     */
+    private void getUser(String uid) {
+        this.binding.loading.setVisibility(View.VISIBLE);
+
+        ParseQuery<User> query = ParseQuery.getQuery(User.class);
+        query.whereEqualTo(User.KEY_OBJECT_ID, uid);
+        query.getFirstInBackground(new GetCallback<User>() {
+            @Override
+            public void done(User object, ParseException e) {
+                if(e == null) {
+                    user = object;
+                    bindInformation();
+                    getPlaces();
+                } else {
+                    Toast.makeText(context, "User not found", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Binds the information of the logged user into the page
+     */
+    private void bindInformation() {
+        this.binding.tvName.setText(this.user.getName());
+        this.binding.tvUsername.setText(String.format("@%s", this.user.getUsername()));
+        String imageUrl;
+        try {
+            imageUrl = user.getProfilePicture().getUrl();
+        } catch (NullPointerException e) {
+            imageUrl = "";
+            Log.e(TAG, "No image for the user");
+        }
+        Glide.with(this)
+                .load(imageUrl)
+                .placeholder(R.drawable.avatar)
+                .error(R.drawable.avatar)
+                .circleCrop()
+                .into(this.binding.ivProfilePicture);
+        this.binding.tvBio.setText(this.user.getBio());
+
+        // Get followers and following count
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Relations");
+        query.whereEqualTo("user", this.user);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject _object, ParseException e) {
+                if(e == null) {
+                    followerCount = _object.getInt("followerCount");
+                    followingCount = _object.getInt("followingCount");
+                    binding.tvFollowersCount.setText(String.format("FOLLOWERS: %d", followerCount));
+                    binding.tvFollowingCount.setText(String.format("FOLLOWERS: %d", followingCount));
+                } else {
+                    Log.e(TAG, "Error getting counts", e);
+                }
+            }
+        });
+    }
+
+    /**
+     * Gets the places that the user has uploaded
+     */
+    private void getPlaces() {
+        ParseQuery<Place> query = ParseQuery.getQuery(Place.class);
+        query.whereEqualTo("user", this.user);
+        query.findInBackground(new FindCallback<Place>() {
+            @Override
+            public void done(List<Place> _places, ParseException e) {
+                if(e == null) {
+                    places.addAll(_places);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Log.e(TAG, "Error while getting places", e);
+                }
+
+                binding.loading.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void onUpdateTextInformation(Context context, String title, String data) {
+        final EditText taskEditText = new EditText(context);
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setView(taskEditText)
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputText = String.valueOf(taskEditText.getText());
+
+                        if(data.equals("name")) {
+                            user.setName(inputText);
+                            user.saveInBackground();
+                            binding.tvName.setText(inputText);
+                        } else if(data.equals("bio")) {
+                            user.setBio(inputText);
+                            user.saveInBackground();
+                            binding.tvBio.setText(inputText);
+                        }
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .create();
+        dialog.show();
     }
 
 }
