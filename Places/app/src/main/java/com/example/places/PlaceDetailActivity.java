88ea2f2cca93c329b71b5b37d96cada09dd4d35a
Pablo Blanco <pabloblanco@fb.com>
2021-07-15 12:14:34 -0500

    place edit actions for owner implemented

--- Places/app/src/main/java/com/example/places/PlaceDetailActivity.java
+++ Places/app/src/main/java/com/example/places/PlaceDetailActivity.java
@@ -12,9 +12,18 @@ import com.bumptech.glide.Glide;
 import com.example.places.databinding.ActivityPlaceDetailBinding;
 import com.example.places.models.Place;
 import com.example.places.models.User;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.DeleteCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class PlaceDetailActivity extends AppCompatActivity {
 
@@ -24,6 +33,9 @@ public class PlaceDetailActivity extends AppCompatActivity {
     // Attributes
     private ActivityPlaceDetailBinding binding;
     private Place place;
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private int deleteStep = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +50,44 @@ public class PlaceDetailActivity extends AppCompatActivity {
         String uid = intent.getStringExtra("place");
         this.getPlace(uid);
 
+        // Setup map with WorkaroundFragment so that drag & move still work
+        mapFragment = ((WorkaroundMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    loadMap(map);
+                }
+            });
+        } else {
+            Toast.makeText(this, "Error loading map", Toast.LENGTH_SHORT).show();
+        }
 
+
+    }
+
+    /**
+     * Loads the map
+     * Uses the special class WorkaroundMapFragment to intercept any touchEvent from
+     *  ScrollView so that when touching the map the screen stays the same.
+     * @param googleMap: the map from the layout
+     */
+    protected void loadMap(GoogleMap googleMap) {
+        this.map = googleMap;
+        if (this.map == null) {
+            Toast.makeText(this, "Error loading map", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Set map settings, and create listener to intercept clicks so that map is able to move
+        this.map.getUiSettings().setZoomControlsEnabled(false);
+        ((WorkaroundMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .setListener(new WorkaroundMapFragment.OnTouchListener() {
+                    @Override
+                    public void onTouch() {
+                        binding.scrollView.requestDisallowInterceptTouchEvent(true);
+                    }
+                });
     }
 
     /**
@@ -78,6 +127,16 @@ public class PlaceDetailActivity extends AppCompatActivity {
         this.binding.tvCategory.setText(this.place.getCategory().getString("name"));
         this.binding.rbPrice.setRating(this.place.getPrice());
 
+        // Add marker to map
+        LatLng placePosition = new LatLng(this.place.getLat(), this.place.getLng());
+        this.map.addMarker(new MarkerOptions()
+                .position(placePosition)
+                .title(this.place.getName()));
+
+        // Move camera to marker
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(placePosition, 17);
+        this.map.animateCamera(cameraUpdate);
+
         // Load image
         Glide.with(PlaceDetailActivity.this)
                 .load(this.place.getImage().getUrl())
@@ -86,7 +145,74 @@ public class PlaceDetailActivity extends AppCompatActivity {
                 .centerCrop()
                 .into(this.binding.ivImage);
 
+        // If author is same as logged user then display edit actions
+        if(this.place.getUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
+            this.binding.rlAuthor.setVisibility(View.GONE);
+            this.binding.rlEdit.setVisibility(View.VISIBLE);
+        } else {
+            this.binding.rlAuthor.setVisibility(View.VISIBLE);
+            this.binding.rlEdit.setVisibility(View.GONE);
+        }
+
+        // Set edit actions
+        if(this.place.getPublic()) {
+            this.binding.btnPublic.setText(R.string.make_private);
+        } else {
+            this.binding.btnPublic.setText(R.string.make_public);
+        }
+        this.binding.btnPublic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePlacePrivacy();
+            }
+        });
+
+        // Set delete listener
+        this.binding.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deletePlace();
+            }
+        });
+
 
         this.binding.loading.setVisibility(View.GONE);
+    }
+
+    /**
+     * Deletes the place
+     */
+    private void deletePlace() {
+        if(this.deleteStep == 0) {
+            this.binding.btnDelete.setText(R.string.sure_question);
+            this.deleteStep++;
+        } else {
+            this.deleteStep = 0;
+            this.place.deleteInBackground(new DeleteCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null) {
+                        Toast.makeText(PlaceDetailActivity.this, "Place deleted", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(PlaceDetailActivity.this, "Could not delete place", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Changes the privacy of the place (public <-> private)
+     */
+    private void changePlacePrivacy() {
+        this.place.setPublic(!this.place.getPublic());
+        this.place.saveInBackground();
+
+        if(this.place.getPublic()) {
+            this.binding.btnPublic.setText(R.string.make_private);
+        } else {
+            this.binding.btnPublic.setText(R.string.make_public);
+        }
     }
 }
