d65f0ab3f9d83cc6eb1766e029923add707422dc
Pablo Blanco <pabloblanco@fb.com>
2021-07-15 18:51:32 -0500

    :rocket: Feed implemented

---
+++ Places/app/src/main/java/com/example/places/adapters/FeedAdapter.java
@@ -0,0 +1,140 @@
+package com.example.places.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.places.PlaceDetailActivity;
+import com.example.places.ProfileActivity;
+import com.example.places.R;
+import com.example.places.models.Place;
+import com.example.places.models.SearchResult;
+import com.example.places.models.User;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+
+    // Attributes
+    private Context context;
+    private List<Place> places;
+
+    public FeedAdapter(Context context, List<Place> places) {
+        this.context = context;
+        this.places = places;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(this.context).inflate(R.layout.item_place_feed, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Place place = this.places.get(position);
+        holder.bind(place);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.places.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Attributes
+        private ImageView ivImage, ivUserImage;
+        private TextView tvAuthorName, tvUsername, tvName, tvCategory;
+        private RelativeLayout rlAuthor, rlPlace;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Find items on View
+            this.ivImage = itemView.findViewById(R.id.ivImage);
+            this.ivUserImage = itemView.findViewById(R.id.ivUserImage);
+            this.tvName = itemView.findViewById(R.id.tvName);
+            this.tvAuthorName = itemView.findViewById(R.id.tvAuthorName);
+            this.tvUsername = itemView.findViewById(R.id.tvUsername);
+            this.tvCategory = itemView.findViewById(R.id.tvCategory);
+            this.rlAuthor = itemView.findViewById(R.id.rlAuthor);
+            this.rlPlace = itemView.findViewById(R.id.rlPlace);
+        }
+
+        /**
+         * Bind the information from the place to it's view
+         * @param place: The place which information wants to be displayed
+         */
+        public void bind(Place place) {
+            // Bind place's information
+            this.tvName.setText(place.getName());
+            this.tvCategory.setText(String.format("CATEGORY: %s", place.getCategory().get("name")));
+
+            String imageUrl;
+            try {
+                imageUrl = place.getImage().getUrl();
+            } catch (NullPointerException e) {
+                imageUrl = "";
+            }
+            Glide.with(context)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .centerCrop()
+                    .into(ivImage);
+
+            // Bind user's information
+            ParseUser user = place.getUser();
+            this.tvUsername.setText(String.format("@%s",user.getUsername()));
+            this.tvAuthorName.setText(user.getString(User.KEY_NAME));
+
+            String authorImage;
+            try {
+                authorImage = user.getParseFile(User.KEY_PROFILE_PICTURE).getUrl();
+            } catch (NullPointerException e) {
+                authorImage = "";
+            }
+            Glide.with(context)
+                    .load(authorImage)
+                    .placeholder(R.drawable.avatar)
+                    .error(R.drawable.avatar)
+                    .circleCrop()
+                    .into(ivUserImage);
+
+
+            // Click listener to go to author
+            this.rlAuthor.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ProfileActivity.class);
+                    intent.putExtra("user", user.getObjectId());
+                    context.startActivity(intent);
+                }
+            });
+
+            // Click listener to go to place's detail
+            this.rlPlace.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, PlaceDetailActivity.class);
+                    intent.putExtra("place", place.getObjectId());
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+    }
+
+}
--- Places/app/src/main/java/com/example/places/ui/home/HomeFragment.java
+++ Places/app/src/main/java/com/example/places/ui/home/HomeFragment.java
@@ -1,39 +1,106 @@
 package com.example.places.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.places.R;
+import com.example.places.adapters.FeedAdapter;
 import com.example.places.databinding.FragmentHomeBinding;
+import com.example.places.models.Place;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    // Constants
+    private static final String TAG = "HomeFragment";
+
+    // Attributes
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private Context context;
+    private List<Place> places;
+    private FeedAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
+        // Set up binding and context
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        this.context = getContext();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        // Create and setup adapter
+        this.places = new ArrayList<>();
+        this.adapter = new FeedAdapter(context, this.places);
+        this.binding.rvPlaces.setAdapter(this.adapter);
+        this.binding.rvPlaces.setLayoutManager(new LinearLayoutManager(context));
+
+        // Query your feed
+        getFeed();
+
+        return root;
+    }
+
+    /**
+     * Get the most current posts from your feed
+     */
+    private void getFeed() {
+        binding.loading.setVisibility(View.VISIBLE);
+
+        ParseQuery<ParseObject> innerQuery = ParseQuery.getQuery("Follower");
+        innerQuery.whereEqualTo("follower", ParseUser.getCurrentUser());
+        innerQuery.include("following");
+
+        ParseQuery<Place> outerQuery = ParseQuery.getQuery(Place.class);
+        outerQuery.orderByDescending(Place.KEY_CREATED_AT);
+        outerQuery.whereMatchesKeyInQuery("user", "following", innerQuery);
+        outerQuery.include(Place.KEY_USER);
+        outerQuery.include(Place.KEY_CATEGORY);
+        outerQuery.whereEqualTo(Place.KEY_PUBLIC, true);
+
+        outerQuery.findInBackground(new FindCallback<Place>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void done(List<Place> objects, ParseException e) {
+                binding.loading.setVisibility(View.GONE);
+
+                if(e == null) {
+                    if(objects.size() == 0) {
+                        binding.tvNoResults.setVisibility(View.VISIBLE);
+                        binding.rvPlaces.setVisibility(View.GONE);
+                    } else {
+                        places.addAll(objects);
+                        adapter.notifyDataSetChanged();
+                        binding.tvNoResults.setVisibility(View.GONE);
+                        binding.rvPlaces.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    Toast.makeText(context, "Error getting timeline", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error getting timeline", e);
+                }
             }
         });
-        return root;
+
     }
 
     @Override
